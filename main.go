e200b848e36094c7d153f2cdeb64e71d7324b4ab
Pedro Henrique <109990820+drohoug@users.noreply.github.com>
2025-01-28 14:06:21 +0000

    init project

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "flag"
+
+func main() {
+	flag.Func("username", "ufg <username> -- string reopresentation of username.", func(v string) error (
+		if v == "" 
+
+	)
+
+	flag.Parse()
+
+}
